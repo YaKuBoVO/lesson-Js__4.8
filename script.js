d3aee06c6eca637f71398f76d50ccfc94b3a2e49
YaKuBoVO <oyakubov526@gmail.com>
2023-07-16 23:46:24 +0500

    ertaga xudo-xoxlasa tugataman

--- script.js
+++ script.js
@@ -1,1 +1,131 @@
 "use strict"
+// Prevent animation an load
+setTimeout(() => {
+   document.body.classList.remove('preload');
+});
+
+let btnRules = document.querySelector('.rules-btn');
+let btnClose = document.querySelector('.close-btn');
+let modalRules = document.querySelector('.modal');
+let resultWinner = document.querySelector('.results__winner');
+let resultText = document.querySelector(".results__text");
+
+const CHOICES = [
+   {
+      name: "paper",
+      beats: "rock"
+   },
+    {
+      name: "scissors",
+      beats: "paper"
+   },
+    {
+      name: "rock",
+      beats: "scissors"
+   },
+]
+const choiceButtons = document.querySelectorAll('.choice-btn');
+const gameDiv = document.querySelector('.game');
+const resultsDiv = document.querySelector('.results');
+const resultDivs = document.querySelectorAll('.results__result');
+
+const playAgainBtn = document.querySelector('.play-again');
+
+const scoreNumber = document.querySelector('.score__number');
+let score = 0;
+
+// Game Logic
+choiceButtons.forEach(button => {
+   button.addEventListener('click', () => {
+      const choiceName = button.dataset.choice;
+      const choice = CHOICES.find(choice => choice.name === choiceName);
+      choose(choice)
+   })
+})
+
+function choose(choice) {
+   const aichoice = aiChoose()
+   displayResults([choice, aichoice]);
+   displayWinner([choice, aichoice]);
+}
+
+function aiChoose() {
+   const rand = Math.floor(Math.random() * CHOICES.length);
+   return CHOICES[rand];
+}
+
+function displayResults(results) {
+   resultDivs.forEach((resultDiv, idx) => {
+      setTimeout(() => {
+         resultDiv.innerHTML = `
+         <div class="choice ${results[idx].name}">
+         <img src="img/dont-${results[idx].name}.svg" alt="${results[idx].name}"/>
+         </div>
+         `
+      }, idx * 1000);
+   });
+   gameDiv.classList.toggle('hidden');
+   resultsDiv.classList.toggle('hidden');
+}
+
+//resultDivs.style.width = "160px";
+//resultDivs.style.height = "160px";
+
+
+
+function displayWinner(results) {
+   setTimeout(() => {
+      const userWins = isWinner(results);
+      const aiWins = isWinner(results.reverse());
+
+      if(userWins) {
+         resultText.innerText = "you win";
+         resultDivs[0].classList.toggle('winner');
+         keepScore(1);
+      }  else if(aiWins) {
+         resultText.innerText = "you lose";
+          resultDivs[1].classList.toggle('winner');
+          keepScore(-1);
+      }  else {
+         resultText.innerText = "draw"
+      }
+      resultWinner.classList.toggle('hidden');
+      resultsDiv.classList.toggle('show-winner');
+   }, 1000);
+}
+
+
+function isWinner(results) {
+   return results[0].beats === results[1].name;
+}
+
+function keepScore(point) {
+   score += point;
+   scoreNumber.innerText = score;
+}
+
+
+
+// Play again
+playAgainBtn.addEventListener('click', () => {
+   gameDiv.classList.toggle('hidden');
+   resultsDiv.classList.toggle('hidden');
+
+   resultDivs.forEach(resultDiv => {
+      resultDiv.innerHTML = "";
+      resultDiv.classList.remove('winner');
+   });
+
+   resultText.innerText = "";
+   resultWinner.classList.toggle('hidden');
+   resultWinner.classList.toggle('show-winner');
+});
+
+
+// Show/Hide Rules
+btnRules.addEventListener("click", () => {
+   modalRules.classList.toggle("show-modal");
+});
+btnClose.addEventListener("click", () => {
+   modalRules.classList.toggle("show-modal");
+});
